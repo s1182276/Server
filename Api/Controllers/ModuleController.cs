b9bd2373755e773369a6ead7e0af4747bcc61426
tobik <s1178329@student.windesheim.nl>
2024-04-18 11:46:34 +0200

    Working MVC and API Endpoints

--- Api/Controllers/ModuleController.cs
+++ Api/Controllers/ModuleController.cs
@@ -3,6 +3,7 @@ using KeuzeWijzerApi.Models;
 using KeuzeWijzerApi.Services;
 using KeuzeWijzerApi.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KeuzeWijzerApi.Controllers
 {
@@ -10,7 +11,7 @@ namespace KeuzeWijzerApi.Controllers
     [Route("[controller]")]
     public class ModuleController : Controller
     {
-        IModuleRepo moduleRepo;
+        private readonly IModuleRepo moduleRepo;
         private readonly KeuzeWijzerContext _dbcontextD;
 
         public ModuleController(KeuzeWijzerContext dbcontext)
@@ -19,23 +20,94 @@ namespace KeuzeWijzerApi.Controllers
             _dbcontextD = dbcontext;
         }
 
-        [HttpGet("GetAllModules")]
-        public IActionResult GetAllModules()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Module>>> GetModules()
         {
-            var allModules = _dbcontextD.Modules.ToList();
-            return Ok(allModules);
+            if (_dbcontextD == null) return NotFound();
+            if (_dbcontextD.Modules == null) return NotFound();
+            return await _dbcontextD.Modules.ToListAsync(); ;
         }
 
-        [HttpPost("CreateModule")]
-        public IActionResult SaveModule([FromBody] Models.Module module)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Module>> GetModule(int id)
         {
-            Module moduleDto = new Module();
-            moduleDto.Name = module.Name;
-            moduleDto.Description = module.Description;
+            var module = await _dbcontextD.Modules.FindAsync(id);
 
-            _dbcontextD.Modules.Add(moduleDto);
-            _dbcontextD.SaveChanges();
-            return Ok();
+            if (module == null)
+            {
+                return NotFound();
+            }
+
+            return module;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModule(int id, Module module)
+        {
+            if (id != module.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbcontextD.Entry(module).State = EntityState.Modified;
+
+            try
+            {
+                await _dbcontextD.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModuleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Module>> PostModule(Module module)
+        {
+            _dbcontextD.Modules.Add(module);
+            await _dbcontextD.SaveChangesAsync();
+
+            return CreatedAtAction("GetModule", new { id = module.Id }, module);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModule(int id)
+        {
+            var module = await _dbcontextD.Modules.FindAsync(id);
+            if (module == null)
+            {
+                return NotFound();
+            }
+
+            _dbcontextD.Modules.Remove(module);
+            await _dbcontextD.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //public IActionResult SaveModule([FromBody] Models.Module module)
+        //{
+        //    Module moduleDto = new Module();
+        //    moduleDto.Name = module.Name;
+        //    moduleDto.Description = module.Description;
+
+        //    _dbcontextD.Modules.Add(moduleDto);
+        //    _dbcontextD.SaveChanges();
+        //    return Ok();
+        //}
+
+        private bool ModuleExists(int id)
+        {
+            return _dbcontextD.Modules.Any(e => e.Id == id);
         }
     }
 }
